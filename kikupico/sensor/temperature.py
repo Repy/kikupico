78b1e8a63f36bbf71925c8d65e724c017cab919d
Repy <gamecube02+git@gmail.com>
2024-06-29 18:33:30 +0900

    気温センサー完成

--- kikupico/sensor/temperature.py
+++ kikupico/sensor/temperature.py
@@ -1,59 +1,95 @@
 import machine
 import time
 import ustruct
-import micropython
+
 class Temperature:
-    def __init__(self, temperature, pressure, humidity):
+    def __init__(self, temperature, pressure, humidity, raw):
         self.temperature = temperature
         self.pressure = pressure
         self.humidity = humidity
+        self.raw = raw
 
+    def __print(self):
+        print(self.raw.hex())
+        print(self.temperature)
+        print(self.pressure)
+        print(self.humidity)
+
+    def __str__(self):
+        return "{:.2f} {:.2f} {:.2f}".format(self.temperature, self.pressure, self.humidity)
+    
 class CalibrationData:
     def __init__(self, data88, dataE1):
         (self.t1, self.t2, self.t3) = ustruct.unpack("<Hhh", data88[0:6])
         (self.p1, self.p2, self.p3, self.p4, self.p5, self.p6, self.p7, self.p8, self.p9) = ustruct.unpack("<Hhhhhhhhh", data88[6:24])
-        (self.h1) = ustruct.unpack("<B", data88[25:26])
-        (self.h2, self.h3) = ustruct.unpack("<hB", data88[0:3])
-        (h4,) = ustruct.unpack("<b", data88[3:4])
-        self.h4 = (h4 << 4) | (data88[4] & 0xF)
-        (h5,) = ustruct.unpack("<b", data88[5:6])
-        self.h5 = (h5 << 4) | (data88[4] >> 4)
-        (self.h6) = ustruct.unpack("<b", dataE1[6:7])
+        (self.h1,) = ustruct.unpack("<B", data88[25:26])
+        (self.h2, self.h3) = ustruct.unpack("<hB", dataE1[0:3])
+        (h4,) = ustruct.unpack("<b", dataE1[3:4])
+        self.h4 = (h4 << 4) | (dataE1[4] & 0xF)
+        (h5,) = ustruct.unpack("<b", dataE1[5:6])
+        self.h5 = (h5 << 4) | (dataE1[4] >> 4)
+        (self.h6,) = ustruct.unpack("<b", dataE1[6:7])
 
-    def compensate_temperature(self, adc_T, adc_P, adc_H):
+    def __print(self):
+        print(self.t1, self.t2, self.t3)
+        print(self.p1, self.p2, self.p3, self.p4, self.p5, self.p6, self.p7, self.p8, self.p9)
+        print(self.h1, self.h2, self.h3, self.h4, self.h5, self.h6)
 
-        var1 = ((((adc_T >> 3) - (self.t1 << 1))) * (self.t2)) >> 11
-        var2 = (((((adc_T >> 4) - (self.t1)) * ((adc_T >> 4) - (self.t1))) >> 12) * (self.t3)) >> 14
+    def compensate_temperature(self, data):
+        adc_P = (data[0] << 12) | (data[1] << 4) | (data[2] >> 4)
+        adc_T = (data[3] << 12) | (data[4] << 4) | (data[5] >> 4)
+        adc_H = (data[6] << 8) | data[7]
+
+        var1 = (adc_T / 16384.0 - self.t1 / 1024.0)
+        var1 = var1 * self.t2
+        var2 = adc_T / 131072.0 - self.t1 / 8192.0
+        var2 = (var2 * var2) * self.t3
         t_fine = var1 + var2
-        T = (t_fine * 5 + 128) >> 8
-    
+        temperature = (var1 + var2) / 5120.0
 
-        var1 = t_fine - 128000
-        var2 = var1 * var1 * self.p6
-        var2 = var2 + ((var1 * self.p5) << 17)
-        var2 = var2 + ((self.p4) << 35)
-        var1 = ((var1 * var1 * self.p3) >> 8) + ((var1 * self.p2) << 12)
-        var1 = ((((1) << 47) + var1)) * (self.p1) >> 33
-        if (var1 != 0):
-            p = 1048576 - adc_P
-            p = (((p << 31) - var2) * 3125) // var1
-            var1 = ((self.p9) * (p >> 13) * (p >> 13)) >> 25
-            var2 = ((self.p8) * p) >> 19
-            p = ((p + var1 + var2) >> 8) + ((self.p7) << 4)
+        if (temperature < -40):
+            temperature = -40
+        elif (temperature > 85):
+            temperature = 85
 
-        micropython.mem_info()
-        v_x1_u32r = (t_fine - 76800)
-        v_x1_u32r = (((((adc_H << 14) - ((self.h4) << 20) - ((self.h5) * v_x1_u32r)) + (16384)) >> 15) * (((((((v_x1_u32r * (self.h6)) >> 10) * (((v_x1_u32r * (self.h3)) >> 11) + (32768))) >> 10) + (2097152)) * (self.h2) + 8192) >> 14))
-        micropython.mem_info()
 
-        v_x1_u32r = (v_x1_u32r - (((((v_x1_u32r >> 15) * (v_x1_u32r >> 15)) >> 7) * (self.h1)) >> 4))
-        if v_x1_u32r < 0:
-            v_x1_u32r = 0
-        if v_x1_u32r > 419430400:
-            v_x1_u32r = 419430400
-        H = v_x1_u32r >> 12
+        var1 = (t_fine / 2.0) - 64000.0
+        var2 = var1 * var1 * self.p6 / 32768.0
+        var2 = var2 + var1 * self.p5 * 2.0
+        var2 = (var2 / 4.0) + (self.p4 * 65536.0)
+        var3 = self.p3 * var1 * var1 / 524288.0
+        var1 = (var3 + self.p2 * var1) / 524288.0
+        var1 = (1.0 + var1 / 32768.0) * self.p1
 
-        return Temperature(T / 100, p / 256 / 100, H / 1024)
+        if (var1 > (0.0)):
+            pressure = 1048576.0 - adc_P
+            pressure = (pressure - (var2 / 4096.0)) * 6250.0 / var1
+            var1 = self.p9 * pressure * pressure / 2147483648.0
+            var2 = pressure * self.p8 / 32768.0
+            pressure = pressure + (var1 + var2 + self.p7) / 16.0
+            if (pressure < 30000.0):
+                pressure = 30000.0
+            elif (pressure > 110000.0):
+                pressure = 110000.0
+        else:
+            pressure = 30000.0
+
+
+        var1 = t_fine - 76800.0
+        var2 = (self.h4 * 64.0 + (self.h5 / 16384.0) * var1)
+        var3 = adc_H - var2
+        var4 = self.h2 / 65536.0
+        var5 = 1.0 + (self.h3 / 67108864.0) * var1
+        var6 = 1.0 + (self.h6 / 67108864.0) * var1 * var5
+        var6 = var3 * var4 * (var5 * var6)
+        humidity = var6 * (1.0 - self.h1 * var6 / 524288.0)
+
+        if humidity < 0:
+            humidity = 0
+        if humidity > 100:
+            humidity = 100
+
+        return Temperature(temperature, pressure / 100, humidity, data)
         # return Temperature(T / 100, p / 256 / 100, 0)
 
 class TemperatureSensor:
@@ -127,16 +163,7 @@ class TemperatureSensor:
         while self.__get_status():
             time.sleep(0.1)            
         data = self.__read_bytes(0xF7, 8)
-        press_raw = self.__byte3(data[0:3])
-        temp_raw = self.__byte3(data[3:6])
-        humi_raw = (data[6] << 8) | data[7]
-        return self.calibration_data.compensate_temperature(temp_raw, press_raw, humi_raw)
-
-    def __byte3(self, data):
-        data_msb = data[0] << 12
-        data_lsb = data[1] << 4
-        data_xlsb = data[2] >> 0
-        return data_msb | data_lsb | data_xlsb
+        return self.calibration_data.compensate_temperature(data)
 
 MODE_SLEEP = 0x00
 MODE_NORMAL = 0x03
