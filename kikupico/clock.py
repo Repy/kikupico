27be30ea5950bd1fd670a866423a6064723be1b2
Repy <gamecube02+git@gmail.com>
2024-07-03 08:23:20 +0900

    時刻計算機能追加

--- kikupico/clock.py
+++ kikupico/clock.py
@@ -7,18 +7,34 @@ ntptime.host = "ntp.nict.jp"
 
 
 class Time:
-    year = 2020
+    __epoch = 0
+    year = 1970
     month = 12
     date = 31
     hour = 23
     minute = 58
     second = 59
 
+    def __init__(self, epoch):
+        self.set(epoch)
+
     def __str__(self):
         return "{:04d}/{:02d}/{:02d} {:02d}:{:02d}:{:02d}".format(
             self.year, self.month, self.date, self.hour, self.minute, self.second
         )
 
+    def set(self, epoch):
+        self.__epoch = epoch
+        now = time.gmtime(epoch + 9 * 60 * 60)  # type: ignore
+        self.year = now[0]
+        self.month = now[1]
+        self.date = now[2]
+        self.hour = now[3]
+        self.minute = now[4]
+        self.second = now[5]
+
+    def add(self, second):
+        self.set(self.__epoch + second)
 
 def init():
     try:
@@ -28,14 +44,7 @@ def init():
 
 
 def get():
-    now = time.localtime(time.mktime(time.localtime()) + 9 * 60 * 60)  # type: ignore
-    t = Time()
-    t.year = now[0]
-    t.month = now[1]
-    t.date = now[2]
-    t.hour = now[3]
-    t.minute = now[4]
-    t.second = now[5]
+    t = Time(time.time())
     return t
 
 
