d0be9708104b13dcf00bba1268986b6256050cab
Repy <gamecube02+git@gmail.com>
2024-08-21 09:01:23 +0900

    timeout修正

--- kikupico/wifi.py
+++ kikupico/wifi.py
@@ -2,36 +2,41 @@ import network
 import machine
 from . import led
 from . import logging
+from . import clock
 
 ssid = ""
 key = ""
 
-nic = None
+nic = network.WLAN(network.STA_IF)
 wifipin = machine.Pin(23, machine.Pin.OUT)
 
 __retry = 3
 __timeout = 15
 
+
 def connect():
     try:
-        wifipin.high()
-        nic = network.WLAN(network.STA_IF)
-        nic.active(True)
         for i in range(__retry):
+            wifipin.high()
+            nic.active(True)
             nic.connect(ssid, key)
             for j in range(__timeout):
                 led.tick(1, 0.5)
                 if nic.status() == network.STAT_GOT_IP:
                     return
-        raise Exception("Failed to connect: "+str(nic.status()))
+            nic.disconnect()
+            nic.active(False)
+            wifipin.low()
+            led.tick(1, 0.5)
+        raise Exception("Failed to connect: " + str(nic.status()))
     except Exception as e:
         logging.error(e)
+
 
 def disconnect():
     try:
         nic.disconnect()
         nic.active(False)
-        nic = None
         wifipin.low()
     except Exception as e:
         logging.error(e)
